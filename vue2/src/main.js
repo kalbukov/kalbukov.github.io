5f9219dfb133545bea24936d9aa2ce56bbb4f8a8
Denis Golinzovsky <680682@gmail.com>
2017-08-04 15:14:33 +0300

    vue2.7

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -24,11 +24,11 @@ Vue.use(Carousel3d)
 
 
 const routes = [
-  { path: '/vue2/', component: App },
-  { path: '/vue2/dota2', component: Dota2 },
-  { path: '/vue2/lol', component: Lol },
-  { path: '/vue2/hots', component: Hots },
-  { name: 'hero', path: '/vue2/hots-:HeroId-:Test', component: HotsHero }
+  { path: '/', component: App },
+  { path: '/dota2', component: Dota2 },
+  { path: '/lol', component: Lol },
+  { path: '/hots', component: Hots },
+  { name: 'hero', path: '/hots-:HeroId-:Test', component: HotsHero }
 ]
 
 
